b84b90e376c29042a02ad6d8a5fae8318ff4a2de
Mathew <doakmath@gmail.com>
2025-01-12 06:09:12 -0600

    fixed / in url

--- frontend/src/Auth0ProviderWithHistory.js
+++ frontend/src/Auth0ProviderWithHistory.js
@@ -7,7 +7,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}auth0-config/`)
+    axios.get(`${process.env.REACT_APP_API_URL}/auth0-config/`)
       .then(response => {
         setAuthConfig(response.data);
       })
